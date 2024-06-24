7b6c923f7124261e87b8e2d8dc62f03575ceba40
acerapa <harvey.aparece.work@gmail.com>
2024-06-25 01:53:02 +0800

    Initial Commits

---
+++ frontend/src/components/shared/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="h-[60px] w-full bg-sub flex justify-center">
+    <div class="max-w-7xl h-full w-full text-main flex items-center justify-between px-4">
+      <div class="flex gap-10 items-center">
+        <p class="text-xl font-bold cursor-pointer">Job Hunt</p>
+        <div class="flex gap-5">
+          <RouterLink class="text-sm font-normal">Find Job</RouterLink>
+          <RouterLink class="text-sm font-normal">Post Job</RouterLink>
+          <RouterLink class="text-sm font-normal">About Us</RouterLink>
+          <RouterLink class="text-sm font-normal">Contact Us</RouterLink>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        main: '#31473A',
+        sub: '#EDF4F2'
+      }
+    }
+  },
+  plugins: []
+}
