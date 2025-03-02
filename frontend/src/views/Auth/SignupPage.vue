585fcd907a97e4bcf3da90900a4cda8d19e48b44
acerapa <harvey.aparece.work@gmail.com>
2025-03-02 22:36:10 +0800

    fixes sign in and sign up pages UI

--- frontend/src/views/Auth/SignupPage.vue
+++ frontend/src/views/Auth/SignupPage.vue
@@ -6,50 +6,9 @@
       v-model="showAlert"
       v-if="showAlert"
     />
-    <div class="w-full h-full bg-green-100 flex items-center max-[700px]:hidden">
-      <div class="relative w-[80%] aspect-square max-w-[1175px] max-h-[80%] mx-auto">
-        <img
-          src="@/assets/images/looking-for-job.png"
-          class="absolute left-0 top-10 w-1/4"
-          alt="looking-for-job"
-        />
-        <img
-          src="@/assets/images/happy-man-with-headphones.png"
-          class="absolute right-0 top-10 w-1/5"
-          alt="happy-man-with-headphones"
-        />
-        <img
-          src="@/assets/images/join-now.png"
-          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2"
-          alt="cloud"
-        />
-        <img
-          src="@/assets/images/happy-man-jumping.png"
-          class="absolute bottom-0 w-1/4"
-          alt="happy-man-jumping"
-        />
-        <img
-          src="@/assets/images/already-working.png"
-          class="absolute right-5 bottom-0 w-1/4"
-          alt="already-working"
-        />
-        <img
-          src="@/assets/images/trial-paper-plane.png"
-          class="absolute left-[2%] top-[35%] w-1/6"
-          alt="trial-paper-plane"
-        />
-        <img
-          src="@/assets/images/trial-paper-plane.png"
-          class="absolute bottom-0 w-1/6 right-[2%] rotate-[160deg] top-[35%]"
-          alt="trial-paper-plane"
-        />
-      </div>
-    </div>
-    <div
-      class="min-w-[150px] w-full min-[700px]:max-w-[450px] bg-white h-full flex items-center relative"
-    >
+    <div class="sign-up-wrapper">
       <LoadingComponent v-if="isLoading" />
-      <div class="w-[70%] mx-auto">
+      <div class="sign-up-inner-wrap">
         <div class="flex flex-col items-center">
           <p class="text-main text-base">Sign up to</p>
           <p class="text-main text-[32px] font-bold">Job Hunt</p>
@@ -117,6 +76,13 @@
         </div>
       </div>
     </div>
+    <div class="w-full h-full bg-green-100 flex items-center max-[700px]:hidden">
+      <img
+        class="w-full h-full object-cover object-left"
+        src="@/assets/images/sign-up-cover-photo.png"
+        alt="sign-up-cover-photo.png"
+      />
+    </div>
   </div>
 </template>
 
@@ -182,3 +148,24 @@ const setupErrors = (field: string, schema: ZodSchema, value: any) => {
   modelErrors.value = fieldErrors as UserCreation
 }
 </script>
+
+<style scoped>
+.sign-up-wrapper {
+  @apply min-w-[150px] w-full min-[700px]:max-w-[450px] bg-white h-full flex items-center relative overflow-y-auto;
+}
+
+.sign-up-inner-wrap {
+  @apply w-[70%] mx-auto;
+}
+
+@media screen and (max-height: 700px) {
+  .sign-up-wrapper .sign-up-inner-wrap {
+    max-height: 700px;
+  }
+
+  .sign-up-wrapper {
+    align-items: start;
+    padding: 4rem 0;
+  }
+}
+</style>
